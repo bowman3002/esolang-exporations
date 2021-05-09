8780c330431eb8d22b7065397539d1a01a208416
adumm <austin.dumm@learninga-z.com>
2021-05-09 14:09:41 -0400

    Implement hybrid M/T-c/T-k CPS transform

--- Racket/lisp-compiler.rkt
+++ Racket/lisp-compiler.rkt
@@ -4,18 +4,37 @@
 (require racket/match)
 
 ; Converts atomic expressions (symbols, integers, or lambdas) into atomic CPS expressions
+; When converting a lambda body, uses simple T-c transform to convert directly to
+; this lambda's continuation without wrapping in another lambda
 (define (M expr)
   (match expr
     [`(lambda (,var) ,expr)
      ; =>
      (define $k (gensym '$k))
-     `(lambda (,var ,$k) ,(T-k expr (lambda (rv) `(,$k ,rv))))]
+     `(lambda (,var ,$k) ,(T-c expr $k))]
 
     [(? symbol?) expr]
     [(? integer?) expr]))
 
+; Converts expression and continuation into application of expr into the continuation
+; Used by M when we are transforming a piece of a simple atomic expression
+; Recurses into T-k to handle CPS transform of a more complex nested CPS application
+(define (T-c expr cont)
+  (match expr
+    [`(lambda . ,_)    `(,cont ,(M expr))]
+    [ (? symbol?)      `(,cont ,(M expr))]
+    [ (? integer?)     `(,cont ,(M expr))]
+    [`(,f ,e)
+      ; =>
+      (define $f (gensym '$f))
+      (define $e (gensym '$e))
+      (T-k f (lambda ($f)
+               (T-k e (lambda ($e)
+                        `(,$f ,$e ,cont)))))]))
+
 ; Converts expression and (atomic-expr -> CPS-expr) function into application of the expr into
 ; the continuation
+; Allows calling function to create wrapped continuation holding more complex expressions?
 (define (T-k expr k)
   (match expr
     [`(lambda . ,_)     (k (M expr))]
@@ -28,18 +47,3 @@
      (T-k f (lambda ($f)
             (T-k e (lambda ($e)
                    `(,$f ,$e ,cont)))))]))
-
-; Converts expression and continuation into application of expr into the continuation
-(define (T expr cont)
-  (match expr
-    [`(lambda . ,_)    `(,cont ,(M expr))]
-    [ (? symbol?)      `(,cont ,(M expr))]
-    [ (? integer?)     `(,cont ,(M expr))]
-    [`(,f ,e)
-      ; =>
-      (define $f (gensym '$f))
-      (define $e (gensym '$e))
-      (T f `(lambda (,$f)
-              ,(T e `(lambda (,$e)
-                       (,$f ,$e ,cont)))))]
-      ))
