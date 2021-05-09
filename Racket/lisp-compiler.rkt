19961b53d6b22b50630bf856aa902dc9e2fcd0c7
adumm <austin.dumm@learninga-z.com>
2021-05-09 13:54:23 -0400

    Implement higher-order T function

--- Racket/lisp-compiler.rkt
+++ Racket/lisp-compiler.rkt
@@ -9,10 +9,25 @@
     [`(lambda (,var) ,expr)
      ; =>
      (define $k (gensym '$k))
-     `(lambda (,var ,$k) ,(T expr $k))]
+     `(lambda (,var ,$k) ,(T-k expr (lambda (rv) `(,$k ,rv))))]
 
     [(? symbol?) expr]
     [(? integer?) expr]))
+
+; Converts expression and (atomic-expr -> CPS-expr) function into application of the expr into
+; the continuation
+(define (T-k expr k)
+  (match expr
+    [`(lambda . ,_)     (k (M expr))]
+    [ (? symbol?)       (k (M expr))]
+    [ (? integer?)      (k (M expr))]
+    [`(,f ,e)
+     ; =>
+     (define $rv (gensym '$rv))
+     (define cont `(lambda (,$rv) ,(k $rv)))
+     (T-k f (lambda ($f)
+            (T-k e (lambda ($e)
+                   `(,$f ,$e ,cont)))))]))
 
 ; Converts expression and continuation into application of expr into the continuation
 (define (T expr cont)
@@ -20,7 +35,7 @@
     [`(lambda . ,_)    `(,cont ,(M expr))]
     [ (? symbol?)      `(,cont ,(M expr))]
     [ (? integer?)     `(,cont ,(M expr))]
-    [ `(,f ,e)
+    [`(,f ,e)
       ; =>
       (define $f (gensym '$f))
       (define $e (gensym '$e))
